4155a6f134d845060fc84ca556e1b7a0298ad8bf
Luca Quattrone <54541769+LucaCosimo-Quattrone@users.noreply.github.com>
2019-09-17 17:27:45 +0200

    Update sport.php

--- sport.php
+++ sport.php
@@ -1,17 +1,14 @@
 <?php
 function getUrlContent($url)
 {
-    $curl = curl_init();
-    curl_setopt_array($curl, array( CURLOPT_URL => $url,
-  	                                CURLOPT_RETURNTRANSFER => true,
+    $curl = curl_init($url);
+    curl_setopt_array($curl, array( CURLOPT_RETURNTRANSFER => true,
   	                                CURLOPT_ENCODING => "",
   	                                CURLOPT_MAXREDIRS => 10,
                                     CURLOPT_TIMEOUT => 30,
                                     CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
                                     CURLOPT_CUSTOMREQUEST => "GET",
-                                    CURLOPT_HTTPHEADER => array("x-rapidapi-host: football-livescore.p.rapidapi.com",
-  		                                                          "x-rapidapi-key: e237d77783msh1277417c4197892p118192jsnf7d06f8c7652"
-  	                                                            ),
+                                    CURLOPT_HTTPHEADER => array("cache-control: no-cache"),
                                     ));
   $response = curl_exec($curl);
   $err = curl_error($curl);
